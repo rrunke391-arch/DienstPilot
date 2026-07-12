2cb0f6027dbbb6f85b4a1bf150a5e1f3f2c95722
rrunke391-arch <rrunke391@gmail.com>
2026-07-12 13:29:40 +0200

    Bereinigte Übersichtsleiste neu laden

--- src/signout.js
+++ src/signout.js
@@ -104,7 +104,7 @@
     loadStylesheet('dpPasswordEyeSlash', 'src/password-eye-slash.css?v=20260711-1');
     loadStylesheet('dpCatalogFieldsHidden', 'src/catalog-fields-hidden.css?v=20260711-2');
     loadStylesheet('dpOverviewPolishCss', 'src/overview-polish.css?v=20260712-1');
-    loadScript('dpOverviewPolish', 'src/overview-polish.js?v=20260712-5');
+    loadScript('dpOverviewPolish', 'src/overview-polish.js?v=20260712-6');
     loadStylesheet('dpDutyCardSimpleCss', 'src/duty-card-simple.css?v=20260712-1');
     loadStylesheet('dpMobileLayoutCss', 'src/mobile-layout.css?v=20260712-1');
     loadScript('dpDutyCardSimple', 'src/duty-card-simple.js?v=20260712-2');
