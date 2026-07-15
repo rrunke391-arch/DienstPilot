f6edf588962a53b40b6b87650d007b7846bcd098
rrunke391-arch <rrunke391@gmail.com>
2026-07-15 11:09:28 +0200

    Fahrer-Wochendruck laden

--- src/driver-home.js
+++ src/driver-home.js
@@ -1,10 +1,15 @@
 (() => {
   'use strict';
 
-  if (document.getElementById('dpDriverHomeV2Script')) return;
-  const script = document.createElement('script');
-  script.id = 'dpDriverHomeV2Script';
-  script.src = 'src/driver-home-v2.js?v=20260713-1';
-  script.async = false;
-  document.head.appendChild(script);
+  function load(id, src) {
+    if (document.getElementById(id)) return;
+    const script = document.createElement('script');
+    script.id = id;
+    script.src = src;
+    script.async = false;
+    document.head.appendChild(script);
+  }
+
+  load('dpDriverHomeV2Script', 'src/driver-home-v2.js?v=20260713-1');
+  load('dpDriverCurrentWeekPrintScript', 'src/driver-current-week-print.js?v=20260715-1');
 })();
