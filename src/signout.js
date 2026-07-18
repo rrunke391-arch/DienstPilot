1d1a9ac8f2549629e1cf28eb760c12844017412e
rrunke391-arch <rrunke391@gmail.com>
2026-07-18 11:08:49 +0200

    Runke-Import und korrigierte Kalenderwochen laden

--- src/signout.js
+++ src/signout.js
@@ -101,7 +101,8 @@
 
   function loadUserModules() {
     loadScript('dpPlanApiBridge', 'src/plan-api-bridge.js?v=20260712-1');
-    loadScript('dpRunkePlanRollback20260813To20261009', 'src/runke-plan-2026-08-13-to-10-09.js?v=20260718-2');
+    loadScript('dpRunkePlan20260813To20261009V3', 'src/runke-plan-2026-08-13-to-10-09.js?v=20260718-3');
+    loadScript('dpRunkeWeekDisplayFix', 'src/runke-week-display-fix.js?v=20260718-1');
     loadScript('dpVacationPersistenceV4', 'src/vacation-persistence-v4.js?v=20260713-2');
     loadScript('dpDriverVacationAccess', 'src/driver-vacation-access.js?v=20260713-1');
     loadScript('dpDriverHomeScript', 'src/driver-home.js?v=20260715-1');
