75148bd87ba06df9020a092371ad082865f1223e
rrunke391-arch <rrunke391@gmail.com>
2026-07-19 14:13:30 +0200

    Sichere Dienstplanspeicherung vor allen Planmodulen laden

--- src/signout.js
+++ src/signout.js
@@ -101,6 +101,7 @@
 
   function loadUserModules() {
     loadScript('dpPlanApiBridge', 'src/plan-api-bridge.js?v=20260712-1');
+    loadScript('dpDailyDutyStorageGuardV1', 'src/daily-duty-storage-guard.js?v=20260719-1');
     loadScript('dpRalfRunkePlan20260813To20261009V6', 'src/runke-plan-2026-08-13-to-10-09.js?v=20260718-6');
     loadScript('dpRunkeWeekDisplayFix', 'src/runke-week-display-fix.js?v=20260718-1');
     loadScript('dpVacationPersistenceV4', 'src/vacation-persistence-v4.js?v=20260713-2');
